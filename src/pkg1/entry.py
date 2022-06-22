93e132caa9a630f1330bf70433b636077be2938d
PhilippSchuette <p.schuette@online.de>
2022-06-22 19:04:39 +0200

    add entry points and dependencies to pip build. extend github actions build.

---
+++ src/pkg1/entry.py
@@ -0,0 +1,8 @@
+"""
+A simple example for a cli entry script called `pyzeal`, c.f. setup.cfg.
+"""
+
+
+def greeting() -> None:
+    "This functions does nothing useful."
+    print("Welcome to PyZEAL")
