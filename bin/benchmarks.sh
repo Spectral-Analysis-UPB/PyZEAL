23f5ea32c5e2611af83e6ff8141cec3f580277af
PhilippSchuette <p.schuette@online.de>
2023-01-17 12:02:39 +0100

    create benchmarking utility in bin/.

---
+++ bin/benchmarks.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd ../
+
+if [[ "$1" != "--benchmark" && "$1" != "--preview" ]]
+then
+    echo "usage: --preview   (start browser preview for existing benchmarks)"
+    echo "       --benchmark (benchmark the most recent commit)"
+fi
+
+if [[ "$1" == "--benchmark" || "$2" == "--benchmark" ]]
+then
+    echo "[asv] running benchmarks for pyzeal..."
+    asv run
+    asv publish
+fi
+
+if [[ "$1" == "--preview" || "$2" == "--preview" ]]
+then
+    echo "[asv] starting browser preview of benchmarks..." 
+    asv preview
+fi
