6d61f47ca63a34b927374c56c517a18d04b3c577
PhilippSchuette <p.schuette@online.de>
2023-03-29 00:05:46 +0200

    update diagrams.sh.

--- bin/diagrams.sh
+++ bin/diagrams.sh
@@ -7,17 +7,17 @@ echo "| Generating Class and Package Diagrams |"
 echo "|---------------------------------------|"
 echo
 
-OPTS="--colorized --color-palette darkseagreen,gold,chocolate --only-classnames -a0"
+OPTS="--colorized --color-palette darkseagreen,gold,chocolate,hotpink --only-classnames -a0"
 OUTPUT="--output dot"
-TARGET="pyzeal.rootfinders pyzeal.algorithms pyzeal.pyzeal_logging"
+TARGET="pyzeal.rootfinders pyzeal.algorithms pyzeal.pyzeal_logging pyzeal.utils"
 
-echo "[pyreverse]  targeting [" $TARGET "]..."
+echo "[pyreverse]  targeting {"$TARGET"}..."
 pyreverse $OPTS $OUTPUT $TARGET 1>/dev/null
 
-echo "[dot]        converting results to pdf..."
+echo "[dot      ]  converting results to pdf..."
 dot -Tpdf -O packages.dot classes.dot
 
-echo "[cleanup]    moving results to docs/_static"
+echo "[cleanup  ]  moving results to docs/_static..."
 rm classes.dot packages.dot
 mv classes.dot.pdf docs/_static/class_diagram1.pdf
 mv packages.dot.pdf docs/_static/package_diagram1.pdf
