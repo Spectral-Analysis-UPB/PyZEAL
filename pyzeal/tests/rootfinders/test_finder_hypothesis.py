4d5db222d58953f9ce76d8d145d10affc7c50238
PhilippSchuette <p.schuette@online.de>
2023-04-04 15:53:49 +0200

    try fix hypothesis tests by increasing search rectangle.

--- pyzeal/tests/rootfinders/test_finder_hypothesis.py
+++ pyzeal/tests/rootfinders/test_finder_hypothesis.py
@@ -76,7 +76,7 @@ def testSimpleArgumentNewtonHypothesis(
 
     hrf.setRootFilter(filterType=FilterTypes.FUNCTION_VALUE_ZERO)
     hrf.setRootFilter(filterType=FilterTypes.ZERO_IN_BOUNDS)
-    hrf.calculateRoots((-5.1, 5.2), (-5.3, 5.4), precision=(5, 5))
+    hrf.calculateRoots((-5.8, 5.9), (-6.0, 6.1), precision=(5, 5))
 
     foundRoots = np.sort_complex(hrf.roots)
     expectedRoots = np.sort_complex(np.array(uniqueRoots))
